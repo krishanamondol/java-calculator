c11084b492d077445d790f49ea5d1100e2525a27
krishanamondol <mail.krishanamondal@gmail.com>
2022-03-29 02:22:20 +0600

    first commit

---
+++ git.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class git {
+public static void main(String[] args) {
+    // System.out.println("Well come to learn with mondal");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("hello Bangladesh\nIntroduace to digital calcutor system");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = (a + b);
+    System.out.println("Value of sum :"+sum);
+    int rediuse = (a - b);
+    System.out.println("Value of Reduace :"+rediuse);
+    int multiply = (a * b);
+    System.out.println("Value of Multiply : "+multiply);
+    int devaide =(a / b);
+    double myDouble = devaide;
+    System.out.println("Value of Devaide : "+myDouble);
+
+    }
+    
+}
